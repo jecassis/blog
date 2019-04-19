201455e3084a51e0254669b635994bd111470a40
Jimmy Cassis <KernelOops@outlook.com>
2019-04-18 22:24:04 -0700

    Fix iconv-loader.js critical dependency build hang

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const webpack = require('webpack');
+
 module.exports = {
     target: 'serverless',
+    webpack: (config) => {
+        config.plugins.push(
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^\.\/iconv-loader$/,
+                contextRegExp: /lib$/,
+            }),
+        );
+
+        return config;
+    },
 };
